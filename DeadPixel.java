c1c0e7b70daf34078e9e3c485bd5d715a3ea4a18
bag-man <garland.owen@gmail.com>
2014-04-06 00:11:55 +0000

    Adding all the files

---
+++ DeadPixel.java
@@ -0,0 +1,22 @@
+import javax.swing.JDialog;
+import java.awt.Color;
+import java.util.Random;
+
+public class DeadPixel extends JDialog {
+
+  public DeadPixel(int x, int y) {
+
+    x = 0 + (int)(Math.random() * ((x - 0) + 1));
+    y = 0 + (int)(Math.random() * ((y - 0) + 1));
+    setType(Type.UTILITY);
+    setSize(1, 1);
+    setLocation(x,y);
+    setResizable(false); 
+    setUndecorated(true);
+    setDefaultCloseOperation(DISPOSE_ON_CLOSE);  
+    setAlwaysOnTop(true);
+    getContentPane().setBackground(Color.BLACK);
+
+  }
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ DeadPixel
 =========
 
 A java app, that randomly "kills" a pixel on your screen. 
+
+I was bored and was wondering if I could make an app that seemingly kills a pixel on your screen. 
+
+If you were more mischevious you could perhaps make this install itself on someones computer and kill a specific pixel. Or even kill pixels randomly during usage.
+
+I noticed it was really memory heavy on Windows which was a bit odd. 
