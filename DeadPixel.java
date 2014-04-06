66208ba2032c8a1f8025b403eed32046645ca6f3
bag-man <garland.owen@gmail.com>
2014-04-06 00:28:21 +0000

    Made it pixel sized

--- DeadPixel.java
+++ DeadPixel.java
@@ -9,7 +9,7 @@ public class DeadPixel extends JDialog {
     x = 0 + (int)(Math.random() * ((x - 0) + 1));
     y = 0 + (int)(Math.random() * ((y - 0) + 1));
     setType(Type.UTILITY);
-    setSize(10, 10);
+    setSize(1, 1);
     setLocation(x,y);
     setResizable(false); 
     setUndecorated(true);
